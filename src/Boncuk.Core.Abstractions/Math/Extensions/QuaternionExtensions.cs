c766079c36dc04abedb91f38802f911c22d2ffc2
Steffen Roßberg <srossberg@gmx.net>
2022-01-06 12:53:29 +0100

    added QuaternionExtensions

---
+++ src/Boncuk.Core.Abstractions/Math/Extensions/QuaternionExtensions.cs
@@ -0,0 +1,16 @@
+namespace Boncuk.Core.Abstractions.Math.Extensions;
+
+public static class QuaternionExtensions
+{
+    public static Quaternion Add(this Quaternion left, Quaternion right)
+        => new (left.X + right.X, left.Y + right.Y, left.Z + right.Z, left.W + right.W);
+
+    public static Quaternion Subtract(this Quaternion left, Quaternion right)
+        => new (left.X - right.X, left.Y - right.Y, left.Z - right.Z, left.W - right.W);
+
+    public static Quaternion Multiply(this Quaternion left, Quaternion right)
+        => new (left.X * right.X, left.Y * right.Y, left.Z * right.Z, left.W * right.W);
+
+    public static Quaternion Divide(this Quaternion left, Quaternion right)
+        => new (left.X / right.X, left.Y / right.Y, left.Z / right.Z, left.W / right.W);
+}
---
+++ src/UnitTests.Boncuk.Core.Abstractions/Math/Extensions/QuaternionExtensionsFacts.cs
@@ -0,0 +1,92 @@
+using Boncuk.Core.Abstractions.Math;
+using Boncuk.Core.Abstractions.Math.Extensions;
+using Xunit;
+
+namespace UnitTests.Boncuk.Core.Abstractions.Math.Extensions;
+
+public class QuaternionExtensionsFacts
+{
+    [Theory]
+    [InlineData(1F, 2F, 3F, 4F, 3F, 4F, 5F, 6F, 1F + 3F, 2F + 4F, 3F + 5F, 4F + 6F)]
+    [InlineData(2F, 3F, 4F, 5F, 4F, 5F, 6F, 7F, 2F + 4F, 3F + 5F, 4F + 6F, 5F + 7F)]
+    [InlineData(-1F, -2F, -3F, -4F, -3F, -4F, -5F, -6F, -1F + -3F, -2F + -4F, -3F + -5F, -4F + -6F)]
+    [InlineData(-2F, -3F, -4F, -5F, -4F, -5F, -6F, -7F, -2F + -4F, -3F + -5F, -4F + -6F, -5F + -7F)]
+    public void Adds_quaternions(float x1, float y1, float z1, float w1, float x2, float y2, float z2, float w2, float expectedX, float expectedY, float expectedZ, float expectedW)
+    {
+        // Given
+        var sut = new Quaternion(x1, y1, z1, w1);
+        var source2 = new Quaternion(x2, y2, z2, w2);
+        
+        // When
+        var actual = sut.Add(source2);
+
+        // Then
+        Assert.Equal(expectedX, actual.X);
+        Assert.Equal(expectedY, actual.Y);
+        Assert.Equal(expectedZ, actual.Z);
+        Assert.Equal(expectedW, actual.W);
+    }
+    
+    [Theory]
+    [InlineData(1F, 2F, 3F, 4F, 3F, 4F, 5F, 6F, 1F - 3F, 2F - 4F, 3F - 5F, 4F - 6F)]
+    [InlineData(2F, 3F, 4F, 5F, 4F, 5F, 6F, 7F, 2F - 4F, 3F - 5F, 4F - 6F, 5F - 7F)]
+    [InlineData(-1F, -2F, -3F, -4F, -3F, -4F, -5F, -6F, -1F - -3F, -2F - -4F, -3F - -5F, -4F - -6F)]
+    [InlineData(-2F, -3F, -4F, -5F, -4F, -5F, -6F, -7F, -2F - -4F, -3F - -5F, -4F - -6F, -5F - -7F)]
+    public void Subtracts_quaternions(float x1, float y1, float z1, float w1, float x2, float y2, float z2, float w2, float expectedX, float expectedY, float expectedZ, float expectedW)
+    {
+        // Given
+        var sut = new Quaternion(x1, y1, z1, w1);
+        var source2 = new Quaternion(x2, y2, z2, w2);
+        
+        // When
+        var actual = sut.Subtract(source2);
+
+        // Then
+        Assert.Equal(expectedX, actual.X);
+        Assert.Equal(expectedY, actual.Y);
+        Assert.Equal(expectedZ, actual.Z);
+        Assert.Equal(expectedW, actual.W);
+    }
+    
+    [Theory]
+    [InlineData(1F, 2F, 3F, 4F, 3F, 4F, 5F, 6F, 1F * 3F, 2F * 4F, 3F * 5F, 4F * 6F)]
+    [InlineData(2F, 3F, 4F, 5F, 4F, 5F, 6F, 7F, 2F * 4F, 3F * 5F, 4F * 6F, 5F * 7F)]
+    [InlineData(-1F, -2F, -3F, -4F, -3F, -4F, -5F, -6F, -1F * -3F, -2F * -4F, -3F * -5F, -4F * -6F)]
+    [InlineData(-2F, -3F, -4F, -5F, -4F, -5F, -6F, -7F, -2F * -4F, -3F * -5F, -4F * -6F, -5F * -7F)]
+    public void Multiplies_quaternions(float x1, float y1, float z1, float w1, float x2, float y2, float z2, float w2, float expectedX, float expectedY, float expectedZ, float expectedW)
+    {
+        // Given
+        var sut = new Quaternion(x1, y1, z1, w1);
+        var source2 = new Quaternion(x2, y2, z2, w2);
+        
+        // When
+        var actual = sut.Multiply(source2);
+
+        // Then
+        Assert.Equal(expectedX, actual.X);
+        Assert.Equal(expectedY, actual.Y);
+        Assert.Equal(expectedZ, actual.Z);
+        Assert.Equal(expectedW, actual.W);
+    }
+    
+    [Theory]
+    [InlineData(1F, 2F, 3F, 4F, 3F, 4F, 5F, 6F, 1F / 3F, 2F / 4F, 3F / 5F, 4F / 6F)]
+    [InlineData(2F, 3F, 4F, 5F, 4F, 5F, 6F, 7F, 2F / 4F, 3F / 5F, 4F / 6F, 5F / 7F)]
+    [InlineData(-1F, -2F, -3F, -4F, -3F, -4F, -5F, -6F, -1F / -3F, -2F / -4F, -3F / -5F, -4F / -6F)]
+    [InlineData(-2F, -3F, -4F, -5F, -4F, -5F, -6F, -7F, -2F / -4F, -3F / -5F, -4F / -6F, -5F / -7F)]
+    public void Divides_quaternions(float x1, float y1, float z1, float w1, float x2, float y2, float z2, float w2, float expectedX, float expectedY, float expectedZ, float expectedW)
+    {
+        // Given
+        var sut = new Quaternion(x1, y1, z1, w1);
+        var source2 = new Quaternion(x2, y2, z2, w2);
+    
+        // When
+        var actual = sut.Divide(source2);
+
+        // Then
+        Assert.Equal(expectedX, actual.X);
+        Assert.Equal(expectedY, actual.Y);
+        Assert.Equal(expectedZ, actual.Z);
+        Assert.Equal(expectedW, actual.W);
+    }
+}
