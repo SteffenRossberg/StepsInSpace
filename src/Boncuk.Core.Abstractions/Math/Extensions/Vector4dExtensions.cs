921afcebb5c4a1208f6a15303cb64f0924f5a0ec
Steffen Roßberg <srossberg@gmx.net>
2022-01-06 12:53:04 +0100

    added Vector4dExtensions

---
+++ src/Boncuk.Core.Abstractions/Math/Extensions/Vector4dExtensions.cs
@@ -0,0 +1,16 @@
+namespace Boncuk.Core.Abstractions.Math.Extensions;
+
+public static class Vector4dExtensions
+{
+    public static Vector4d Add(this Vector4d left, Vector4d right)
+        => new (left.X + right.X, left.Y + right.Y, left.Z + right.Z, left.W + right.W);
+
+    public static Vector4d Subtract(this Vector4d left, Vector4d right)
+        => new (left.X - right.X, left.Y - right.Y, left.Z - right.Z, left.W - right.W);
+
+    public static Vector4d Multiply(this Vector4d left, Vector4d right)
+        => new (left.X * right.X, left.Y * right.Y, left.Z * right.Z, left.W * right.W);
+
+    public static Vector4d Divide(this Vector4d left, Vector4d right)
+        => new (left.X / right.X, left.Y / right.Y, left.Z / right.Z, left.W / right.W);
+}
