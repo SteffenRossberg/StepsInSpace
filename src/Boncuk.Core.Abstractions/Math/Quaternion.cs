86cbd517bddd1e448cc30cb2011065b692551977
Steffen Roßberg <srossberg@gmx.net>
2022-01-07 09:26:10 +0100

    added identity

--- src/Boncuk.Core.Abstractions/Math/Quaternion.cs
+++ src/Boncuk.Core.Abstractions/Math/Quaternion.cs
@@ -11,6 +11,7 @@ public readonly struct Quaternion
     public static readonly Quaternion UnitY = new(0.0F, 1.0F, 0.0F, 0.0F);
     public static readonly Quaternion UnitZ = new(0.0F, 0.0F, 1.0F, 0.0F);
     public static readonly Quaternion UnitW = new(0.0F, 0.0F, 0.0F, 1.0F);
+    public static readonly Quaternion Identity = UnitW;
 
     public Quaternion(float x, float y, float z, float w) => (X, Y, Z, W) = (x, y, z, w);
 }
