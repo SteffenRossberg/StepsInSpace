518d71c5915868af01c61bc5f227a75fb06c17dc
Steffen Roßberg <srossberg@gmx.net>
2022-01-07 17:32:41 +0100

    added normalized direction

--- src/Boncuk.Core.Abstractions/Collision/Ray.cs
+++ src/Boncuk.Core.Abstractions/Collision/Ray.cs
@@ -1,4 +1,5 @@
 using Boncuk.Core.Abstractions.Math;
+using Boncuk.Core.Abstractions.Math.Extensions;
 
 namespace Boncuk.Core.Abstractions.Collision;
 
@@ -6,7 +7,8 @@ public readonly struct Ray
 {
     public readonly Vector3d Origin;
     public readonly Vector3d Direction;
+    public readonly Vector3d NormalizedDirection;
 
     public Ray(Vector3d origin, Vector3d direction)
-        => (Origin, Direction) = (origin, direction);
+        => (Origin, Direction, NormalizedDirection) = (origin, direction, direction.Normalize());
 }
