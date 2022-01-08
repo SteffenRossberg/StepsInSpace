33a9f675d057f1826a1820ece978d3a874dcb169
Steffen Roßberg <srossberg@gmx.net>
2022-01-08 11:13:44 +0100

    added Zero and One to vector2d

--- src/Boncuk.Core.Abstractions/Math/Vector2d.cs
+++ src/Boncuk.Core.Abstractions/Math/Vector2d.cs
@@ -7,6 +7,8 @@ public readonly struct Vector2d
     
     public static readonly Vector2d UnitX = new(1.0f, 0.0f);
     public static readonly Vector2d UnitY = new(0.0f, 1.0f);
-    
+    public static readonly Vector2d Zero = new();
+    public static readonly Vector2d One = new(1.0F, 1.0F);
+
     public Vector2d(float x, float y) => (X, Y) = (x, y);
 }
