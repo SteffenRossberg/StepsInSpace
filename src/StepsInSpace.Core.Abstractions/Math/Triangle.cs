0f71828b5e1f9fbd0df2179a595a6f9b4c71d7d6
Steffen Roßberg <srossberg@gmx.net>
2022-01-16 09:43:34 +0100

    added Triangle

---
+++ src/StepsInSpace.Core.Abstractions/Math/Triangle.cs
@@ -0,0 +1,45 @@
+using StepsInSpace.Core.Abstractions.Collision;
+
+namespace StepsInSpace.Core.Abstractions.Math;
+
+public struct Triangle
+{
+    public readonly Vector3d A;
+    public readonly Vector3d B;
+    public readonly Vector3d C;
+    public readonly Plane Plane;
+    public readonly Vector2d TextureA;
+    public readonly Vector2d TextureB;
+    public readonly Vector2d TextureC;
+    
+    public Triangle(
+        Vector3d a,
+        Vector3d b,
+        Vector3d c)
+        : this(
+            a,
+            b,
+            c,
+            Vector2d.Zero,
+            Vector2d.Zero,
+            Vector2d.Zero)
+    {
+    }
+
+    public Triangle(
+        Vector3d a, 
+        Vector3d b, 
+        Vector3d c, 
+        Vector2d textureA, 
+        Vector2d textureB, 
+        Vector2d textureC)
+    {
+        A = a;
+        B = b;
+        C = c;
+        Plane = new Plane(A, B, C);
+        TextureA = textureA;
+        TextureB = textureB;
+        TextureC = textureC;
+    }
+}
